adda122d9b7b58b76fadfcd1b3f87e80cdeccea1
Ngbth <the283378@gmail.com>
2026-06-04 23:12:21 +0700

    Restore QuyTrinhDangKyPage and QuyenLoiPage

---
+++ vietfutureclone/src/pages/QuyTrinhDangKyPage.tsx
@@ -0,0 +1,52 @@
+export default function QuyTrinhDangKyPage() {
+  return (
+    <main className="py-8 bg-[#f8fbff]">
+      <div className="container mx-auto max-w-305 px-4">
+        <div style={{ margin: 'auto', width: '100%' }}>
+          <p className="text-base text-[#140000] text-left font-bold">
+            Bước 1:
+          </p>
+          <ul className="list-disc list-inside space-y-3 text-base text-[#393939] text-left">
+            <li>
+              Đối với các dự án khởi nghiệp đổi mới sáng tạo của sinh viên: Sinh viên nộp các dự án thuộc 14 lĩnh vực của Giải thưởng, các dự án R&D mong muốn thương mại hoá cho Trường sơ loại. Mỗi dự án được đăng ký tối đa 3 lĩnh vực. Không giới hạn số lượng tác phẩm dự thi của một tác giả. Số lượng sinh viên mỗi nhóm tham gia từ 2-5 thành viên. Các dự án được hướng dẫn bởi Giảng viên hướng dẫn hoặc mời mentor là lãnh đạo doanh nghiệp hướng dẫn thông qua đăng ký với Ban tổ chức. Quý trường sơ loại, lựa chọn tối đa 03 đề cử cho mỗi lĩnh vực tham gia vòng Chung tuyển toàn quốc.
+            </li>
+            <li>
+              Đối với các dự án theo đặt hàng của doanh nghiệp: Quý trường lựa chọn các nhóm sinh viên xuất sắc, thành lập nhóm dự án để triển khai dự án theo bài toán từ doanh nghiệp. Doanh nghiệp đặt hàng sẽ cử mentor hướng dẫn nhóm dự án. Số lượng sinh viên các nhóm tham gia dự án không giới hạn.
+            </li>
+          </ul>
+
+          <p className="text-base text-[#140000] text-left font-bold mt-6">
+            Bước 2:
+          </p>
+          <ul className="list-disc list-inside space-y-3 text-base text-[#393939] text-left">
+            <li>
+              Sinh viên tạo hồ sơ cá nhân (profile/CV) bằng cách đăng ký tài khoản trên nền tảng InnoConnect: <a href="https://innoconnect.vn/" target="_blank" rel="noopener noreferrer" className="text-[#0057B7] hover:underline">https://innoconnect.vn/</a>
+            </li>
+            <li>
+              Quý trường gửi danh sách sinh viên tham gia Giải thưởng kèm hồ sơ cá nhân (profile/CV) của sinh viên trên nền tảng InnoConnect: <a href="https://innoconnect.vn/" target="_blank" rel="noopener noreferrer" className="text-[#0057B7] hover:underline">https://innoconnect.vn/</a> cho Ban tổ chức bằng cách điền Phiếu đăng ký tham dự Giải thưởng, (Tải phiếu đăng ký <a href="https://docs.google.com/document/d/1RnZLQ7mxdLBYw5az3BLYbnJips10oSU-/edit?usp=sharing&amp;ouid=104863109346634476957&amp;rtpof=true&amp;sd=true" target="_blank" rel="noopener noreferrer" className="text-[#0057B7] hover:underline">TẠI ĐÂY</a>)
+            </li>
+          </ul>
+
+          <p className="text-base text-[#140000] text-left font-bold mt-6">
+            Bước 3:
+          </p>
+          <p className="text-base text-[#393939] text-left">
+            Các dự án sau khi được Trường sơ tuyển sẽ nộp hồ sơ tham gia vòng Chung tuyển toàn quốc tại link: <a href="https://vietfuture.vinasa.org.vn/" target="_blank" rel="noopener noreferrer" className="text-[#0057B7] hover:underline">https://vietfuture.vinasa.org.vn/</a>. Hạn nộp hồ sơ: 10/08/2026
+          </p>
+          <p className="text-base text-[#140000] text-left font-bold mt-6">
+            11/8/2026 – 20/8/2026:
+          </p>
+          <p className="text-base text-[#393939] text-left">
+            Ban tổ chức Sơ loại hồ sơ
+          </p>
+          <p className="text-base text-[#140000] text-left font-bold mt-4">
+            24/08/2026:
+          </p>
+          <p className="text-base text-[#393939] text-left">
+            Thông báo kết quả sơ loại thông qua email
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
