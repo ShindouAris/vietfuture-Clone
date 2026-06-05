9445d1908cc2b4df55125e952376557e99677249
Ngbth <the283378@gmail.com>
2026-06-05 21:15:42 +0700

    Sync from upstream/main

--- vietfutureclone/src/routes.tsx
+++ vietfutureclone/src/routes.tsx
@@ -4,6 +4,8 @@ import GioiThieuChung from './pages/gioi_thieu/gioi-thieu-chung';
 import LinhVucDuThi from './pages/gioi_thieu/linh-vuc-du-thi';
 import LichTrinhTrienKhai from './pages/gioi_thieu/lich-trinh-trien-khai';
 import TieuChiDanhGia from './pages/gioi_thieu/tieu-chi-danh-gia';
+import QuyenLoiPage from './pages/tham_gia/QuyenLoiPage';
+import QuyTrinhDangKyPage from './pages/tham_gia/QuyTrinhDangKyPage';
 
 interface RouteConfig {
   path: string;
@@ -12,23 +14,23 @@ interface RouteConfig {
 
 export const routes: RouteConfig[] = [
   { path: '/', element: <HomePage /> },
-  // Trang giới thiệu
+  // Trang gi?i thi?u
   { path: '/gioi-thieu-chung', element: <GioiThieuChung /> },
   { path: '/linh-vuc-du-thi', element: <LinhVucDuThi /> },
   { path: '/tieu-chi-danh-gia', element: <TieuChiDanhGia /> },
   { path: '/lich-trinh-trien-khai', element: <LichTrinhTrienKhai /> },
-  // Trang hội đồng
+  // Trang h?i d?ng
   { path: '/gioi-thieu-hoi-dong', element: <HomePage /> },
   { path: '/giam-khao', element: <HomePage /> },
   { path: '/mentor', element: <HomePage /> },
-  // Trang giải thưởng
+  // Trang gi?i thu?ng
   { path: '/danh-sach-giai-thuong-vietfuture-2025', element: <HomePage /> },
   { path: '/danh-sach-giai-thuong-vietfuture-2024', element: <HomePage /> },
   { path: '/danh-sach-giai-thuong-vietfuture-2023', element: <HomePage /> },
   // Trang tham gia
-  { path: '/quyen-loi', element: <HomePage /> },
-  { path: '/quy-trinh-dang-ky', element: <HomePage /> },
-  // Trang truyền thông
+  { path: '/quyen-loi', element: <QuyenLoiPage /> },
+  { path: '/quy-trinh-dang-ky', element: <QuyTrinhDangKyPage /> },
+  // Trang truy?n th�ng
   { path: '/thong-cao-bao-chi', element: <HomePage /> },
   { path: '/bao-chi-dua-tin', element: <HomePage /> },
   { path: '/faqs', element: <HomePage /> },
