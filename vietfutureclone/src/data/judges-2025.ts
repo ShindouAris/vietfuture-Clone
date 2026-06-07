4ec17bb0ee145d30e4cbcd1336a4584c5122a90f
nielz2852007-maker <nielz2852007@gmail.com>
2026-06-07 11:59:49 +0700

    khanh

---
+++ vietfutureclone/src/data/judges-2025.ts
@@ -0,0 +1,31 @@
+interface Judge {
+  name: string;
+  position: string;
+  img: string;
+}
+
+export const judges2025: Judge[] = [
+  { name: "Ông Nguyễn Đình Thắng", position: "Chủ tịch Hội đồng Giám khảo", img: "https://vietfuture.world/wp-content/uploads/2025/11/nguyendinhthang1.jpg" },
+  { name: "Ông Phí Anh Tuấn", position: "Chủ tịch HĐQT kiêm Tổng giám đốc Công ty tư vấn P.A.T Consulting Ltd", img: "https://vietfuture.world/wp-content/uploads/2025/11/phianhtuan.jpg" },
+  { name: "Ông Lâm Quốc Thái", position: "Quản lý đầu tư tại Quỹ DFJ VinaCapital, VinaCapital Ventures", img: "https://vietfuture.world/wp-content/uploads/2025/11/lamquocthai.jpg" },
+  { name: "Ông Hoàng Lê Minh", position: "Giám đốc AI Tech Center, Đại học Văn Lang", img: "https://vietfuture.world/wp-content/uploads/2025/11/hoangleminh1.jpg" },
+  { name: "Ông Lâm Quang Nam", position: "Phó Chủ tịch VINASA", img: "https://vietfuture.world/wp-content/uploads/2025/11/lamquangnam1-1.jpg" },
+  { name: "Bà Nguyễn Thị Thu Giang", position: "Phó Chủ tịch, Tổng thư ký VINASA", img: "https://vietfuture.world/wp-content/uploads/2025/11/giang2.jpg" },
+  { name: "Ông Louis Nguyễn CEO", position: "Saigon Asset Management (SAM)", img: "https://vietfuture.world/wp-content/uploads/2025/11/louisnguyen.jpg" },
+  { name: "Ông Wilson Lieu", position: "Chủ Tịch, Liên Minh phát triển năng lực AI Việt Nam", img: "https://vietfuture.world/wp-content/uploads/2025/11/wilsonlieu.jpg" },
+  { name: "Ông Kha Phan", position: "Co-founder & CTO tại Easy AI", img: "https://vietfuture.world/wp-content/uploads/2025/11/khaphan.jpg" },
+  { name: "Bà Hoàng Thị Cẩm Ly", position: "CEO Việt Tiến International Education", img: "https://vietfuture.world/wp-content/uploads/2025/11/hoangthicamly2.jpg" },
+  { name: "Bà Lê Mỹ Nga", position: "Founder & CEO WeAngels Capital", img: "https://vietfuture.world/wp-content/uploads/2025/11/lemynga1.jpg" },
+  { name: "Ông Mã Thanh Danh", position: "Phó Tổng Giám đốc Tập đoàn KIDO, Cố vấn Cấp cao Insight Data", img: "https://vietfuture.world/wp-content/uploads/2025/11/mathanhdanh.jpg" },
+  { name: "Ông Nguyễn Bá Diệp", position: "Đồng sáng lập MoMo", img: "https://vietfuture.world/wp-content/uploads/2025/11/nguyenbadiep1.jpg" },
+  { name: "Ông Nguyễn Tiến Huy", position: "CEO, Pencil Group", img: "https://vietfuture.world/wp-content/uploads/2025/11/nguyentienhuy.jpg" },
+  { name: "Ông An Ngọc Thao", position: "Phó Tổng thư ký VINASA", img: "https://vietfuture.world/wp-content/uploads/2025/11/anngocthao.jpg" },
+  { name: "Ông Đỗ Trung Anh", position: "Phó trưởng phòng QL KHCN & HTQT, Học viện CN Bưu chính Viễn thông", img: "https://vietfuture.world/wp-content/uploads/2025/11/dotrunganh.jpg" },
+  { name: "Ông Đỗ Thanh Thái", position: "Chuyên gia phần mềm cao cấp, Robert Bosc", img: "https://vietfuture.world/wp-content/uploads/2025/11/dothanhthai.jpg" },
+  { name: "Bà Lê Uyên Thảo", position: "Innovation Lady Founder AI Leaders Việt Nam", img: "https://vietfuture.world/wp-content/uploads/2025/11/leuyenthao.jpg" },
+  { name: "Ông Lê Hòa Nhã", position: "Phó Tổng Giám Đốc Quỹ FUNDGO - TGĐ Công ty CP đầu tư HVA", img: "https://vietfuture.world/wp-content/uploads/2025/11/lehoanha.jpg" },
+  { name: "Ông Lưu Danh Anh Vũ", position: "Giám Đốc Quốc Gia HCLSoftware", img: "https://vietfuture.world/wp-content/uploads/2025/11/luudanhanhvu-1.jpg" },
+  { name: "Bà Nguyễn Thanh Hiền", position: "Đại diện Đại học Leipzig", img: "https://vietfuture.world/wp-content/uploads/2025/11/thanhhien.jpg" },
+  { name: "Ông Nguyễn Đắc Tình", position: "Founder MarTech Vietnam", img: "https://vietfuture.world/wp-content/uploads/2025/11/nguyendactinh-1.jpg" },
+  { name: "Ông Phạm Tiến Thành", position: "CEO & Co-founder Saigon Technology", img: "https://vietfuture.world/wp-content/uploads/2025/11/phamtienthanh.jpg" },
+];
